eb9b11ca6a8e57d89de85537869fd1f788a95181
Neha Pant <npant03@gmail.com>
2020-04-06 14:49:23 -0700

    test for intake moving quasi working

--- src/test/java/frc/robot/subsystems/MockVictorIntakeTest.java
+++ src/test/java/frc/robot/subsystems/MockVictorIntakeTest.java
@@ -1,39 +1,42 @@
 package frc.robot.subsystems;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.mock;
 
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import frc.robot.subsystems.intake.IntakeSub;
 import frc.robot.subsystems.intake.RunIntake;
 
 import org.junit.*;
+import org.mockito.InjectMocks;
 
 public class MockVictorIntakeTest{
 
      // Arrange
      VictorSPX victor = mock(VictorSPX.class);
 
-     // Act
+     @InjectMocks
      IntakeSub intakeMock = new IntakeSub(victor);
     
     @Test
     public void testConstructor() {
-        // Assert
         assertEquals(false, intakeMock.getInverted());
     }
 
     @Test
     public void runIntakeTest(){
-        double intakePower = 8;
+        double executePower = 10;
         RunIntake runIntake = new RunIntake(intakeMock, .5);
         for(int i = 0; i < 5; i++){
-            // runIntake.execute();
-            intakeMock.setPower(.5);
-            intakePower = intakeMock.getPower();
+            runIntake.execute();
+            executePower = intakeMock.getPower();
         }
-        assertEquals(true, intakePower == 0);
-
+        runIntake.end(false);
+        assertEquals(true, executePower == 0.5);
+        assertEquals(true, intakeMock.getPower() == 0);
     }
 }
